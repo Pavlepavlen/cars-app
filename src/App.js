2be8d1b9b849db2f5353aba0ca43b271b16f3f76
Pavlo <pavelpavlen@gmail.com>
2019-02-13 23:48:21 +0200

    inital commit, setting up project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import styled from "styled-components";
+
+const Styles = styled.div`
+
+
+`;
+
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <Styles>
+      
+      </Styles>
+    )
+  }
+}
+
+export default App;
---
+++ src/carsData.js
@@ -0,0 +1,37 @@
+const carsData = [
+    {
+        id: 1,
+        company: "AUDI",
+        model: "R8",
+        img: "./img/vechiles/audi-r8.jpg",
+        info: "The Audi R8 is a mid-engine, 2-seater sports car, which uses Audi's trademark quattro permanent all-wheel drive system. It was introduced by the German car manufacturer Audi AG in 2006.",
+        logo: "./img/logos/audi-logo.jpg"
+    },
+    {
+        id: 2,
+        company: "BMW",
+        model: "M3",
+        img: "./img/vechiles/bmw-m3.jpg",
+        info: "The BMW M3 is a high-performance version of the 3 Series, developed by BMW's in-house motorsport division, BMW M GmbH. M3 models have been derived from the corresponding generations of the BMW 3 Series.",
+        logo: "./img/logos/bmw-logo.jpg"
+    },
+    {
+        id: 3,
+        company: "AUDI",
+        model: "A7",
+        img: "./img/vechiles/audi-a7.jpg",
+        info: "The Audi A7 is an executive car produced by Audi since 2010. A five-door liftback, it features a sloping roofline with a steeply raked rear window and integrated boot lid (forming the so-called \"Sportback\"), and four frameless doors.",
+        logo: "./img/logos/audi-logo.jpg"
+    },
+    {
+        id: 4,
+        company: "Lamborgini",
+        model: "Avantador",
+        img: "./img/vechiles/lamborgini-aventador.jpg",
+        info: "The Lamborghini Aventador is a mid-engine sports car produced by the Italian automotive manufacturer Lamborghini. In keeping with Lamborghini tradition, the Aventador is named after a fighting bull. Aventador (pronounced [aβentaˈðoɾ]) earned the Trofeo de la Peña La Madroñera for its courage in the arena in Zaragoza, Spain, in 1993.",
+        logo: "./img/logos/lamborgini-logo.jpg"
+    }
+]
+
+export default carsData;
+
